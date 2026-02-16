c252acec9868908e0c54dd2b734dcaf8d17d39a5
skipidarnikov <skipidarnikov@gmail.com>
2026-02-16 14:32:01 +0300

    fix:script2.js

--- src/script2.js
+++ src/script2.js
@@ -1,56 +1,53 @@
-{
-    console.log('task 2');
-    {
-        const a = 1;
-        const b = 2;
-        console.log(`1. ${a>b ? a : a==b ? 'equal' : b}`);
+const a = 1;
+const b = 2;
+console.log(`1. ${a>b ? a : a==b ? 'equal' : b}`);
+
+export function getMonthName(monthStr){
+    const month = parseInt(monthStr);
+    let name = 'INVALID MONTH NUMBER';
+    const monthNames = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    if(month !== undefined &&
+        month &&
+        month - 1 < monthNames.length) {
+        name = monthNames[month - 1];
     }
-
-    export function getMonthName(monthStr){
-        const month = parseInt(monthStr);
-        let name = 'NOT VALID MONTH NUMBER';
-        const monthNames = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December'
-        ];
-        if(month !== undefined &&
-            month &&
-            monthmonth - 1 < monthNames.length) {
-            name = monthNames[month - 1];
-        }
-
-        return name;
-    }
-
-    {
-        const monthNumStr = prompt('enter month number');
-
-        getMonthName(monthNumStr);
-
-        console.log(`${monthName}`);
-    }
-
-    export function checkCircleInSquare(circleArea, squareArea) {
-        const s = Math.sqrt(squareArea);
-        const d = 2 * Math.sqrt(circleArea / Math.PI);
-
-        return d <= s ? true : false;
-    }
-
-    {
-        const circle = 50;
-        const square = 100;
-
-        console.log(checkCircleInSquare(circle, square));
-    }
+    return name;
 }
+
+export function checkCircleInSquare(circleArea, squareArea) {
+    const isValidType = 
+    (param) => {
+        return param !== undefined ||
+            typeof param === 'number' || 
+            isNaN(param) === false;
+    };
+
+    if(!isValidType(circleArea) ||
+        !isValidType(squareArea))
+        return false;
+
+    const s = Math.sqrt(squareArea);
+    const d = 2 * Math.sqrt(circleArea / Math.PI);
+    return d <= s ? true : false;
+}
+
+const monthNumStr = prompt('enter month number');
+const monthName = getMonthName(monthNumStr);
+console.log(`${monthName}`);
+
+const circle = 50;
+const square = 100;
+console.log(checkCircleInSquare(circle, square));
