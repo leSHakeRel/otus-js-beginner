3b2899f57870081e1513fbf9c7ef6d73aa6470ed
AlexS <skipidarnikov@gmail.com>
2026-02-18 23:03:46 +0300

    fix:var name src\srcipt10.js

--- src/script10.js
+++ src/script10.js
@@ -10,11 +10,11 @@ const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const phonePattern = /^(?:\+?\d{1,3}[-.\s]?)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?(?:\d{4}|\d{2}[-.\s]?\d{2})$|^\d{10}$|^\+\d{11}$/;
 
 export function getStringType(str){
-    if (datePattern.test(input)) {
+    if (datePattern.test(str)) {
         return "date";
-    } else if (emailPattern.test(input)) {
+    } else if (emailPattern.test(str)) {
         return "email";
-    } else if (phonePattern.test(input)) {
+    } else if (phonePattern.test(str)) {
         return "phone";
     } else {
         return undefined;
