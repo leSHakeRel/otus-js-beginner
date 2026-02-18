202d0c271f65db61e66307f9f0355dfdb77f73ee
AlexS <skipidarnikov@gmail.com>
2026-02-18 23:01:03 +0300

    fix:add variable check scr\script8.js

--- src/script8.js
+++ src/script8.js
@@ -1,6 +1,8 @@
 export function getDateFromString(dateString){
     const [day, month, year] = dateString.split('.');
-    return new Date(`${year}-${month}-${day}`);
+    if(a && b && c)
+        return new Date(`${year}-${month}-${day}`);
+    return undefined;
 }
 
 export function getDayOfWeek(date){
@@ -11,15 +13,15 @@ export function getMinutesFrom(currentDate){
     return currentDate.getHours() + currentDate.getMinutes();
 }
 
+export function getYounger(fDate, sDate){
+    return fDate.getTime() > sDate.getTime() ? fDate : sDate;
+}
+
 const promptDate = prompt("Enter date (DD.MM.YYYY");
 
 const userDate = getDateFromString(promptDate);
 console.log(getDayOfWeek(userDate));
 
 console.log(getMinutesFrom(new Date(Date.now())));
-
-export function getYounger(fDate, sDate){
-    return fDate.getTime() > sDate.getTime() ? fDate : sDate;
-}
 
 console.log(getYounger(getDateFromString('18.11.1985'), getDateFromString('19.11.1985')))
