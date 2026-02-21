683546934b25678c95f6f1966ae6523852764070
AlexS <skipidarnikov@gmail.com>
2026-02-21 15:47:57 +0300

    fix:fix src\srcipt7.test.js src\script7.js index.html

--- src/script7.js
+++ src/script7.js
@@ -1,54 +1,63 @@
 const maxParagraphCount = 5;
 
-export function checkParagraphs(){
-    const mainContainer = document.querySelector('.container');
-    const paragraphContainer = mainContainer.querySelector('.paragraphs');
-    return paragraphContainer.querySelectorAll('p');
-}
+export function runApp(element) {
 
-export function clearInput(){
-    const tField = document.querySelector('input');
-    tField.value = '';
+    element.innerHTML = `<div class="paragraphs">
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+        <input>
+        <button style="visibility:hidden">push me</button>`;
+
+  function checkParagraphs() {
+    const paragraphContainer = element.querySelector(".paragraphs");
+    return paragraphContainer.querySelectorAll("p");
+  }
+
+  function clearInput() {
+    const tField = element.querySelector("input");
+    tField.value = "";
     tField.focus();
-}
+  }
 
-export function addParagraph(textValue) {
-    if(typeof textValue !== 'string')
-        return;
-    if(textValue.length > 0) {
-        const newParagraph = document.createElement('p');
-        newParagraph.innerText = textValue;
-        const mainContainer = document.querySelector('.container');
-        const paragraphContainer = mainContainer.querySelector('.paragraphs');
-        paragraphContainer.appendChild(newParagraph);
-        const paragraps = Array.from(checkParagraphs());
-        if (paragraps.length >= maxParagraphCount){
-            const pToRemove = paragraps.slice(0, paragraps.length - maxParagraphCount);
-            pToRemove.forEach(element => {
-                element.remove();
-            });
-        }
+  function addParagraph(textValue) {
+    if (typeof textValue !== "string") return;
+    if (textValue.length > 0) {
+      const newParagraph = document.createElement("p");
+      newParagraph.innerText = textValue;
+      const paragraphContainer = element.querySelector(".paragraphs");
+      paragraphContainer.appendChild(newParagraph);
+      const paragraps = Array.from(checkParagraphs());
+      if (paragraps.length >= maxParagraphCount) {
+        const pToRemove = paragraps.slice(
+          0,
+          paragraps.length - maxParagraphCount,
+        );
+        pToRemove.forEach((element) => {
+          element.remove();
+        });
+      }
     }
+  }
+
+  const input = element.querySelector("input");
+  input.addEventListener("input", (event) => {
+    let isHidden = true;
+    if (
+      event !== undefined &&
+      event.target !== undefined &&
+      typeof event.target.value === "string"
+    ) {
+      if (event.target.value.length > 0) isHidden = false;
+    }
+    const button = element.querySelector("button");
+    button.style.visibility = isHidden ? "hidden" : "visible";
+  });
+
+  const button = element.querySelector("button");
+  button.addEventListener("click", () => {
+    addParagraph(element.querySelector("input").value);
+    clearInput();
+  });
 }
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    const input = document.querySelector('input');
-    input.addEventListener('input', (event)=>{
-        let isHidden = true;
-        if(event !== undefined &&
-            event.target !== undefined &&
-            typeof event.target.value === 'string'){
-            if(event.target.value.length > 0)
-                isHidden = false;
-        }
-        const button = document.querySelector('button');
-        button.style.visibility = isHidden ? "hidden" : "visible";
-    });
-    
-    const button = document.querySelector('button');
-    button.addEventListener('click', ()=>{
-        addParagraph(document.querySelector('input').value)
-        clearInput();
-    });
-});
-
--- src/script7.test.js
+++ src/script7.test.js
@@ -1,110 +1,98 @@
-/** 
- * @jest-environment jsdom 
+/**
+ * @jest-environment jsdom
  */
-const fs = require('fs');
-const path = require('path');
 
-describe('test UI', () => {
+import { runApp } from "./script7";
 
-    beforeEach(() => {
-        const filePath = path.resolve(__dirname, '../index.html');
-        const html = fs.readFileSync(filePath, 'utf8');
-        document.body.innerHTML = html;
+describe("test UI", () => {
+  let element;
+  beforeEach(() => {
+    element = document.createElement('div');
+    runApp(element);
+  });
+  [
+    ["is paragraph elements count equal 3", "p", 3],
+    ["is input element elements count equal 1", "input", 1],
+    ["is button element count equal 1", "button", 1],
+  ].forEach(([name, tag, size]) =>
+    it(name, () => {
+      const elements = element.querySelectorAll(tag);
+      expect(elements).toHaveLength(size);
+    }),
+  );
+
+  it("is input empty", () => {
+    const inputElement = element.querySelector("input");
+
+    const inputText = inputElement.value;
+    expect(inputText.length === 0).toBe(true);
+  });
+
+  it("is button hidden", () => {
+    const button = element.querySelector("button");
+    const isHidden = button.style.visibility === "hidden";
+    expect(isHidden).toBe(true);
+  });
+
+  it("is button enabled when text inserted", () => {
+    const inputElement = element.querySelector("input");
+    inputElement.value = "123";
+    inputElement.dispatchEvent(new Event("input"));
+    const button = element.querySelector("button");
+    expect(button.style.visibility).toBe('visible');
+  });
+
+  it("is p count after text inserted and button clicked equal 4", () => {
+    const inputElement = element.querySelector("input");
+    inputElement.value = "123";
+    inputElement.dispatchEvent(new Event("input"));
+    const button = element.querySelector("button");
+    button.click();
+    const elements = element.querySelectorAll("p");
+    expect(elements).toHaveLength(4);
+  });
+
+  it("is input empty after text inserted and button clicked", () => {
+    const event = new Event("DOMContentLoaded");
+    document.dispatchEvent(event);
+    const inputElement = element.querySelector("input");
+    inputElement.value = "123";
+    inputElement.dispatchEvent(new Event("input"));
+    const button = element.querySelector("button");
+    button.click();
+    expect(inputElement.value).toBe("");
+  });
+
+  it("is button hidden after manualy clear text", () => {
+    const inputElement = element.querySelector("input");
+    inputElement.value = "123";
+    inputElement.dispatchEvent(new Event("input"));
+    inputElement.value = "";
+    inputElement.dispatchEvent(new Event("input"));
+
+    const button = element.querySelector("button");
+    const isHidden = button.style.visibility === "hidden";
+    expect(isHidden).toBe(true);
+    expect(inputElement.value).toBe("");
+  });
+
+  it("is paragraph count equal 4 after text inserted and button clicked", () => {
+    ["1", "2", "3", "4", "5"].forEach((text) => {
+      const inputElement = element.querySelector("input");
+      inputElement.value = text;
+      inputElement.dispatchEvent(new Event("input"));
     });
-    [
-        ['is paragraph elements count equal 3',     'p',        3],
-        ['is input element elements count equal 1', 'input',    1],
-        ['is button element count equal 1',         'button',   1]
-    ].forEach(([name, tag, size]) => it(name, () => {
-        const elements = document.querySelectorAll(tag);
-        expect(elements).toHaveLength(size);
-    }));
+    const button = element.querySelector("button");
+    button.click();
+    const elements = element.querySelectorAll("p");
+    expect(elements).toHaveLength(4);
+  });
 
+  it("is paragraph count equal 3 (not changed after launch and button clicked)", () => {
+    const button = element.querySelector("button");
+    button.click();
 
-    it('is input empty', () => {
-        const inputElement = document.querySelector('input');
-
-        const inputText = inputElement.value;
-        expect(inputText.length === 0).toBe(true);
-    });
-
-    it('is button hidden', () => {
-        const button = document.querySelector('button');
-        const isHidden = button.style.visibility === 'hidden';
-        expect(isHidden).toBe(true);
-    });
-
-    it('is button enabled when text inserted', () => {
-        const inputElement = document.querySelector('input');
-        inputElement.value = '123';
-        inputElement.dispatchEvent(new Event('input'));
-
-        const button = document.querySelector('button');
-        const style = window.getComputedStyle(button);
-        expect(style.display).toBe('inline-block');
-    });
-
-    it('is p count after text inserted and button clicked equal 4', () => {
-        const inputElement = document.querySelector('input');
-        inputElement.value = '123';
-        inputElement.dispatchEvent(new Event('input'));
-        setTimeout(() => {
-            const button = document.querySelector('button');
-            button.click();
-            const elements = document.querySelectorAll('p');
-            expect(elements).toHaveLength(4);
-        });
-    });
-
-    it('is input empty after text inserted and button clicked', () => {
-        const event = new Event('DOMContentLoaded');
-        document.dispatchEvent(event);
-        const inputElement = document.querySelector('input');
-        inputElement.value = '123';
-        inputElement.dispatchEvent(new Event('input'));
-        setTimeout(() => {
-            const button = document.querySelector('button');
-            button.click();
-        expect(inputElement.value).toBe('');
-        }, 0);
-    });
-
-    it('is button hidden after manualy clear text', () => {
-        const inputElement = document.querySelector('input');
-        inputElement.value = '123';
-        inputElement.dispatchEvent(new Event('input'));
-        inputElement.value = '';
-        inputElement.dispatchEvent(new Event('input'));
-        
-        const button = document.querySelector('button');
-        const isHidden = button.style.visibility === 'hidden';
-        expect(isHidden).toBe(true);
-        expect(inputElement.value).toBe('');
-    });
-
-    it('is paragraph count equal 4 after text inserted and button clicked', () => {
-        const button = document.querySelector('button');
-        button.click();
-        ['1','2','3','4','5']
-            .forEach(
-                (text) => {
-                    const inputElement = document.querySelector('input');
-                    inputElement.value = text;
-                    inputElement.dispatchEvent(new Event('input'));
-                }
-            );
-        setTimeout(() => {
-            const elements = document.querySelectorAll('p');
-            expect(elements).toHaveLength(4);
-        }, 0);
-    });
-
-    it('is paragraph count equal 3 (not changed after launch and button clicked)', () => {
-        const button = document.querySelector('button');
-        button.click();
-
-        const elements = document.querySelectorAll('p');
-        expect(elements).toHaveLength(3);
-    });
-    
+    const elements = element.querySelectorAll("p");
+    expect(elements).toHaveLength(3);
+  });
 });
