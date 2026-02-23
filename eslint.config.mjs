cb48710f1f742153798bb35f8ed543bb5968805e
AlexS <skipidarnikov@gmail.com>
2026-02-23 11:21:33 +0300

    fix:eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,21 @@
-import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
 
 export default defineConfig([
+  { ignores: ["**/dist/**", "**/coverage/**", "**/node_modules/**"] },
   {
     files: ["./src/*.{js}"],
-    plugins: { js },
-    extends: ["js/recommended"], 
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
       ...js.configs.recommended.rules,
-      "semi": ["error", "always"]
+      semi: ["error", "always"],
+      "prefer-const": "error",
     },
   },
   {
     files: [".src/**/*.test.js"],
-    ...jest.configs['flat/recommended'],
-  },,
+    ...jest.configs["flat/recommended"],
+  },
 ]);
